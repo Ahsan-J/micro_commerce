47f867d8192d6a6ba02878ca7ec8a8d5467e7a86
Ahsan-J <ahsan.ahmed99@ymail.com>
2020-04-22 01:04:40 +0300

    Init for Common in Java Spring Boot

---
+++ common/src/test/java/com/ahsan/microservice/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahsan.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
